74cd89ac17774f5b67bbf7f522bdb4d702951aa5
bradleycrouch <bradley_crouch@hotmail.com>
2024-03-31 20:17:55 +0100

    Update to allow JsonProxySettigs, will allow to enable features in proxy settings

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bradleycrouch/SimpleJson
-# Build directory: /Users/bradleycrouch/SimpleJson/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/bradleycrouch/SimpleJson/cmake-build-debug/UnitTestJson[1]_include.cmake")
-subdirs("_deps/googletest-build")
